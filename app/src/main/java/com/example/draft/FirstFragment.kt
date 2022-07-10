42611c25b3277c7d8e330328304a79efda94faef
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2022-07-10 17:53:54 +0200

    first connection

--- app/src/main/java/com/example/draft/FirstFragment.kt
+++ app/src/main/java/com/example/draft/FirstFragment.kt
@@ -5,38 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import com.example.draft.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class FirstFragment : AppCompatActivity() {
 
-    private var _binding: FragmentFirstBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreate(savedInstanceState:Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.create_account)
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding.root
 
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        }
 
 
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-}
+
+
--- app/src/main/java/com/example/draft/MainActivity.kt
+++ app/src/main/java/com/example/draft/MainActivity.kt
@@ -1,23 +1,23 @@
 package com.example.draft
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.example.draft.databinding.ActivityMainBinding
-import com.example.draft.fragmentsall.Homefrag
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_first);
+        setContentView(R.layout.fragment_second)
 
+    val facebook = findViewById<Button>(R.id.Facebook)
 
+        facebook?.setOnClickListener {
+            val intent = Intent(this,FirstFragment::class.java)
+            startActivity(intent)
+        }
 
     }}
