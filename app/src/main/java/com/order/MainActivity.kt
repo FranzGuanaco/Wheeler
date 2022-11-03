500faadb40e265fd77355c8d915a4cdc7f0adee0
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2022-11-03 09:53:38 +0100

    debbug

--- app/src/main/java/com/order/MainActivity.kt
+++ app/src/main/java/com/order/MainActivity.kt
@@ -2,13 +2,13 @@ package com.order
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.InputType
-import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.example.draft.Game
 import com.example.draft.R
 
-class Main  : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity() {
 
 
 
@@ -20,7 +20,7 @@ class Main  : AppCompatActivity() {
         val progressbar = findViewById<ProgressBar>(R.id.progressBar)
         val converter:  EditText = findViewById (R.id.gamblesum)
         val toast = Toast.makeText(applicationContext, "pas possible", Toast.LENGTH_SHORT)
-        toast.setGravity(2, 90, 0) // pas important pour le moment
+        toast.setGravity(2, 90, 0)
         val newpage = findViewById<Button>(R.id.validation)
         var prog = 0
 
@@ -34,28 +34,21 @@ class Main  : AppCompatActivity() {
 
         {
 
-            // boutton pour choisir la quantité d'argent à mettre
-
             if ( converter.text.toString().toDoubleOrNull() is Double && converter.text.toString().toDouble() < 200 ){
                 with(progressbar) { progress= (converter.text.toString().toInt() * 100 / 200)
-                    // prog c'est la variable qui reprend la quantité d'argent misé par l'utilisateur
                     prog = progress
                 } }
 
             if ( converter.text.toString().toDoubleOrNull() is Double && converter.text.toString().toDouble() > 200 ){
                 toast.show()
-
-                // toast pour informer de la mise maximale (ne fonctionne pas)
             }
         }
 
         newpage?.setOnClickListener(){
             if ( converter.text.toString().toDoubleOrNull() is Double && converter.text.toString().toDouble() < 200 ){
-                val intent = Intent(this,Result::class.java)
+                val intent = Intent(this, Game::class.java)
                 intent.putExtra("jauge", prog)
                 startActivity(intent)
-
-                // intend une fois que la mise a été validé
             }
         }
     }
--- app/src/main/java/com/user/Game.kt
+++ app/src/main/java/com/user/Game.kt
@@ -1,4 +1,98 @@
-package com.user
+package com.example.draft
 
-class Game {
-}
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.graphics.Interpolator
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.provider.ContactsContract
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import com.example.draft.databinding.FragmentFirstBinding
+import com.google.android.material.progressindicator.AnimatorDurationScaleProvider
+import java.text.DateFormat
+import kotlin.random.Random
+import kotlin.random.nextInt
+import kotlin.system.measureTimeMillis
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class Game : AppCompatActivity()
+{
+
+
+    override fun onCreate(savedInstanceState:Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.game_start)
+
+
+        // Receiving activity from MainActivity
+        val intent = intent
+        val name = intent.getIntExtra("jauge", 1)
+        val bar = findViewById<ProgressBar>(R.id.progressBar2)
+        val button = findViewById<Button>(R.id.button)
+        val cursor = findViewById<ImageView>(R.id.imageView4)
+        var text = findViewById<EditText>(R.id.settext2)
+
+
+        bar.max = 100 // remplissage maximale de la barre
+        bar.progress = name // donnée de la MainActivity
+
+
+
+
+        fun test() {
+
+
+            val array  = intArrayOf(1,3,4,22)
+
+            for (n in array){
+
+                if (n == name){
+                    text.setText("hahah")
+
+                    val animations = arrayOf(1000f, -600f).map { translation ->
+                        ObjectAnimator.ofFloat(cursor, "translationX", translation).apply {
+                            duration = 800
+                            repeatCount = 12
+                            repeatMode = ObjectAnimator.REVERSE
+                        }}
+                    val set = AnimatorSet()
+                    set.playTogether(animations)
+                    set.start()
+                }
+
+
+
+                else{
+                    text.setText("$name")}}}
+
+
+
+        button.setOnClickListener(){
+
+            test()
+        }}}
+
+
+
+// faire que la fleche identifie si elle est sur la progressbar ou non
+// faire que la fleche s'arrete aleatoirement => func minuteur
+
+/*
+object : CountDownTimer(random, 1000) {
+    override fun onTick(millisUntilFinished: Long) {
+        text.setText("seconds remaining: " + millisUntilFinished / 1000)
+    }
+    override fun onFinish() {
+        set.cancel()  // annulation du mouvement de la fleche
+    } */
