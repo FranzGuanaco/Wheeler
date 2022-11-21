6a1577be49edfd5989412bdbe35881ebeb904fa8
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2022-11-22 00:46:55 +0100

    starting splash screen animation (objectanimator)

---
+++ app/src/main/java/com/order/Login.kt
@@ -0,0 +1,13 @@
+package com.order
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.draft.R
+
+class Login: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+}}
--- app/src/main/java/com/order/Splash.kt
+++ app/src/main/java/com/order/Splash.kt
@@ -1,11 +1,16 @@
 package com.order
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.ImageView
+import android.widget.ProgressBar
 import com.example.draft.R
+import kotlinx.coroutines.delay
 
 
 class Splash : AppCompatActivity() {
@@ -13,11 +18,28 @@ class Splash : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        val triangle1 = findViewById<ImageView>(R.id.triangleYellow)
+        val triangle2 = findViewById<ImageView>(R.id.triangleYellow2)
+        val triangle3 = findViewById<ImageView>(R.id.triangleRed)
+        val triangle4 = findViewById<ImageView>(R.id.triangleRed2)
+        val triangle5 = findViewById<ImageView>(R.id.triangleGreen)
+        val triangle6 = findViewById<ImageView>(R.id.trianglePurple)
+        val triangle7 = findViewById<ImageView>(R.id.triangleBlue)
+        val triangle8 = findViewById<ImageView>(R.id.triangleBlue2)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000)
-    }
-}
+
+
+
+        val animations = arrayOf(2000f, 1000f).map { translation ->
+            ObjectAnimator.ofFloat(triangle1, "translationX", translation).apply {
+                duration = 1000
+                startDelay = 2000
+
+
+            }}
+
+            val set = AnimatorSet()
+            set.playTogether(animations)
+            set.start()
+
+        }}
