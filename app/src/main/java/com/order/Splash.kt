83ece44a84ceb69728c6eccac7a40f6c6d2e9cdb
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2022-11-23 10:16:45 +0100

    triangle translation x/y done

--- app/src/main/java/com/order/Splash.kt
+++ app/src/main/java/com/order/Splash.kt
@@ -23,13 +23,51 @@ class Splash : AppCompatActivity() {
         val triangle8 = findViewById<ImageView>(R.id.triangleBlue2)
 
 
-        val objectAnimator1: ObjectAnimator =
+
+        val objectAnimator1x: ObjectAnimator =
             ObjectAnimator.ofFloat(triangle1, "translationX", 1000f)
-        val objectAnimator2: ObjectAnimator =
+        val objectAnimator1y: ObjectAnimator =
+            ObjectAnimator.ofFloat(triangle1, "translationY", 10f)
+
+        val objectAnimator2x: ObjectAnimator =
             ObjectAnimator.ofFloat(triangle2, "translationX", -1000f)
+        val objectAnimator2y: ObjectAnimator =
+            ObjectAnimator.ofFloat(triangle2, "translationY", -10f)
+
+        val objectAnimator3x: ObjectAnimator =
+            ObjectAnimator.ofFloat(triangle3, "translationX", 1000f)
+        val objectAnimator3y: ObjectAnimator =
+            ObjectAnimator.ofFloat(triangle3, "translationY", 300f)
+
+        val objectAnimator4x: ObjectAnimator =
+            ObjectAnimator.ofFloat(triangle4, "translationX", -1000f)
+        val objectAnimator4y: ObjectAnimator =
+            ObjectAnimator.ofFloat(triangle4, "translationY", -600f)
+
+        val objectAnimator5x: ObjectAnimator =
+            ObjectAnimator.ofFloat(triangle5, "translationX", 100f)
+        val objectAnimator5y: ObjectAnimator =
+            ObjectAnimator.ofFloat(triangle5, "translationY", 1200f)
+
+        val objectAnimator6x: ObjectAnimator =
+            ObjectAnimator.ofFloat(triangle6, "translationX", 100f)
+        val objectAnimator6y: ObjectAnimator =
+            ObjectAnimator.ofFloat(triangle6, "translationY", -1200f)
+
+        val objectAnimator7x: ObjectAnimator =
+            ObjectAnimator.ofFloat(triangle7, "translationX", 800f)
+        val objectAnimator7y: ObjectAnimator =
+            ObjectAnimator.ofFloat(triangle7, "translationY", -1500f)
+
+        val objectAnimator8x: ObjectAnimator =
+            ObjectAnimator.ofFloat(triangle8, "translationX", -900f)
+        val objectAnimator8y: ObjectAnimator =
+            ObjectAnimator.ofFloat(triangle8, "translationY", 1500f)
+
 
         val animatorSet = AnimatorSet()
-        animatorSet.playTogether(objectAnimator1, objectAnimator2)
+        animatorSet.playTogether(objectAnimator1x, objectAnimator1y, objectAnimator2x, objectAnimator2y, objectAnimator3x, objectAnimator3y, objectAnimator4x, objectAnimator4y, objectAnimator5x, objectAnimator5y,
+                objectAnimator6x, objectAnimator6y, objectAnimator7x, objectAnimator7y, objectAnimator8x, objectAnimator8y)
         animatorSet.startDelay = 1000
         animatorSet.duration = 2000
 
