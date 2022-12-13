2b16b2e3767e8bdc53ef695e0ed96083699822c9
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2022-12-13 23:39:29 +0100

    gradle implement dependencies lifecycle

--- app/src/main/java/com/ViewModel/Create_account.kt
+++ app/src/main/java/com/ViewModel/Create_account.kt
@@ -1,13 +1,14 @@
 package com.ViewModel
 
+import android.app.Application
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.AndroidViewModel
 import com.example.wheeler.R
 
-class Create_account: AppCompatActivity() {
+class Create_account(application: Application): AndroidViewModel(application) {
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.create_account)
+     fun onCreate(savedInstanceState: Bundle?) {
+
     }}
