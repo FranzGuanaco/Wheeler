fd951b59c67ec43b44135a2f14f3998c4e632245
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2022-12-15 00:58:45 +0100

    test livedata

--- app/src/main/java/com/View/Login.kt
+++ app/src/main/java/com/View/Login.kt
@@ -1,71 +1,36 @@
-package com.ViewModel
+package com.View
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.view.ViewDebug
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.Model.DBHelper
+import com.ViewModel.LoginViewModel
 import com.example.wheeler.R
 
 class Login: AppCompatActivity() {
 
+    lateinit var viewModel: ViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_substitute)
-
-        val password = findViewById<EditText>(R.id.password)
-        val id = findViewById<EditText>(R.id.user_name)
-        val Name = findViewById<TextView>(R.id.Name)
-        val Age = findViewById<TextView>(R.id.Age)
-        val addName = findViewById<Button>(R.id.add)
-        val printName = findViewById<Button>(R.id.print)
+        val test = findViewById<EditText>(R.id.user_name)
+        val button = findViewById<Button>(R.id.add)
 
 
-        addName.setOnClickListener{
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        (viewModel as LoginViewModel).currentnum.observe(this, Observer { test.setText(it.toString()) })
 
-            // below we have created a new DBHelper class, and passed context to it
-            val db = DBHelper(this, null)
 
-            // creating variables for values in name and age edit texts
-            val Pass = password.text.toString()
-            val ID = id.text.toString()
+    button.setOnClickListener()
+    {
+        var uno = 1
 
-            // calling method to add name to our database
-            db.AddData(Pass, ID)
+        (viewModel as LoginViewModel).currentnum.value = uno++
 
-            // Toast to message on the screen
-            Toast.makeText(this, Pass + " added to database", Toast.LENGTH_LONG).show()
 
-            // at last, clearing edit texts
-            password.text.clear()
-            id.text.clear()
-        }
-
-        // below code is to add on  click listener to our print name button
-        printName.setOnClickListener{
-
-            // creating a DBHelper class and passing context to it
-            val db = DBHelper(this, null)
-
-            // below is the variable for cursor we have called method to get all names from our database and add to name text view
-            val cursor = db.getName()
-
-            // moving the cursor to first position and appending value in the text view
-            cursor!!.moveToFirst()
-            Name.append(cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.ID)) + "\n")
-            Age.append(cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.PASSWORD)) + "\n")
-
-            // moving our cursor to next position and appending values
-            while(cursor.moveToNext()){
-                Name.append(cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.ID)) + "\n")
-                Age.append(cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.PASSWORD)) + "\n")
-            }
-
-            // at last we close our cursor
-            cursor.close()
-        }
-    }
-}
+    }}}
--- app/src/main/java/com/ViewModel/LoginViewModel.kt
+++ app/src/main/java/com/ViewModel/LoginViewModel.kt
@@ -1,4 +1,19 @@
 package com.ViewModel
 
-class LoginViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel: ViewModel() {
+
+    var num = 1
+
+
+    val currentnum: MutableLiveData<Int> by lazy { MutableLiveData<Int>() }
+    val currentbool: MutableLiveData<Boolean> by lazy { MutableLiveData<Boolean>() }
+
+
+    fun essai (): Int {
+        num++
+        return num
+    }
 }
