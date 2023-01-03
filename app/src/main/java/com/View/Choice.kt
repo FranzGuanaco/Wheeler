43e2ee5da9aceb67758a5f39cc218aae0db69a3a
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2023-01-03 16:06:33 +0100

    fragment choice

---
+++ app/src/main/java/com/View/Choice.kt
@@ -0,0 +1,22 @@
+package com.View
+
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import com.example.wheeler.R
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class Choice : AppCompatActivity()
+{
+
+
+    override fun onCreate(savedInstanceState:Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.choice)
+
+    }}
