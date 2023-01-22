7b4f2c7923d0b5d453c307e7d61d8be8bd88d9ba
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2023-01-22 23:06:23 +0100

    new prize

--- app/src/main/java/com/View/Choice.kt
+++ app/src/main/java/com/View/Choice.kt
@@ -2,6 +2,7 @@ package com.View
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
@@ -10,7 +11,13 @@ import com.example.wheeler.databinding.ActivityChoiceBinding
 import com.example.wheeler.databinding.ActivityCreateAccountBinding
 import com.example.wheeler.databinding.ActivityPhoneBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
 import com.google.firebase.storage.FirebaseStorage
+import com.squareup.picasso.Picasso
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -28,7 +35,7 @@ class Choice : AppCompatActivity()
         binding = ActivityChoiceBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var prize = MainActivity()
+        var database = FirebaseDatabase.getInstance()
         var img = intent.getStringExtra("img")
 
         auth = FirebaseAuth.getInstance()
--- app/src/main/java/com/View/MainActivity.kt
+++ app/src/main/java/com/View/MainActivity.kt
@@ -15,8 +15,6 @@ import com.google.firebase.storage.FirebaseStorage
 
  class MainActivity : AppCompatActivity() {
 
-//BINDING ???
-
         private lateinit var databaseReference: DatabaseReference
         private lateinit var storageReference: DatabaseReference
         lateinit var binding: ActivityPrizeBinding
@@ -28,114 +26,235 @@ import com.google.firebase.storage.FirebaseStorage
              binding = ActivityPrizeBinding.inflate(layoutInflater)
             setContentView(binding.root)
 
-            var GooglePay = findViewById<ImageView>(R.id.GooglePay)
-            var Stayprize = findViewById<ImageView>(R.id.StayPrize)
-            var NetflixPrize = findViewById<ImageView>(R.id.NetflixPrize)
-            var PrimePrize = findViewById<ImageView>(R.id.PrimePrize)
-            var WeekendPrize = findViewById<ImageView>(R.id.WeekendPrize)
-            var other_2 = findViewById<ImageView>(R.id.other2)
-            var other_1 = findViewById<ImageView>(R.id.other1)
-            var other_3 = findViewById<ImageView>(R.id.other3)
-            var SpotifyPrize = findViewById<ImageView>(R.id.SpotifyPrize)
-            var Rio = findViewById<ImageView>(R.id.Rio)
-            var Mexico = findViewById<ImageView>(R.id.Mexico)
-            var VideoGame = findViewById<ImageView>(R.id.VideoGame)
-            var logout = findViewById<ImageButton>(R.id.logout)
 
-
-            cinema()
+            netflix()
+            Thread.sleep(10)
+            googleplay()
+            Thread.sleep(10)
+            prime()
+            Thread.sleep(10)
+            spotify()
+            Thread.sleep(10)
+            travel()
+            Thread.sleep(10)
+            tokyo()
+            Thread.sleep(10)
+            london()
+            Thread.sleep(10)
+            castle()
+            Thread.sleep(10)
+            best1()
+            Thread.sleep(10)
+            best2()
+            Thread.sleep(10)
+            best3()
+            Thread.sleep(10)
+            best4()
 
             var test = Create_account()
             test.auth = FirebaseAuth.getInstance()
 
-            logout.setOnClickListener(){
+            binding.logout.setOnClickListener(){
                 test.out()
                 var intent = Intent(this, Create_account::class.java)
                 startActivity(intent)
 
             }
 
-            cinema()
-            Thread.sleep(10)
 
 
             var imageRef = FirebaseStorage.getInstance().reference
 
-            var googlePlay =
-                "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2FStore.jpeg?alt=media&token=5c50a9ab-07b2-460b-b45f-0beb7c9eb056"
-            Glide.with(baseContext).asBitmap().load(googlePlay).into(GooglePay)
 
-            Thread.sleep(10)
+            binding.NetflixPrize.setOnClickListener(){
 
-            var stay =
-                "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fstay_prize.jpeg?alt=media&token=9c6f0296-64b0-4997-b7b4-97cc45f9efa2"
-            Glide.with(baseContext).asBitmap().load(stay).into(Stayprize)
-
-            Thread.sleep(10)
-
-
-            var xbox =
-                "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fxbox_price.jpeg?alt=media&token=a2a52487-2ecb-4c4b-a5f6-a67656258d60"
-            Glide.with(baseContext).asBitmap().load(xbox).into(PrimePrize)
-
-            Thread.sleep(10)
-
-            var castle =
-                "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fcastle.jpeg?alt=media&token=b97b65a9-8968-4983-b465-a3e36622c84d"
-            Glide.with(baseContext).asBitmap().load(castle).into(WeekendPrize)
-
-            var other1 =
-                "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fcastle.jpeg?alt=media&token=b97b65a9-8968-4983-b465-a3e36622c84d"
-            Glide.with(baseContext).asBitmap().load(other1).into(other_2)
-
-
-            var other2 =
-                "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fcastle.jpeg?alt=media&token=b97b65a9-8968-4983-b465-a3e36622c84d"
-            Glide.with(baseContext).asBitmap().load(other2).into(other_1)
-
-
-            var other3 =
-                "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fcastle.jpeg?alt=media&token=b97b65a9-8968-4983-b465-a3e36622c84d"
-            Glide.with(baseContext).asBitmap().load(other3).into(other_3)
-
-            Thread.sleep(10)
-
-            var spotify =
-                "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fstay_prize.jpeg?alt=media&token=9c6f0296-64b0-4997-b7b4-97cc45f9efa2"
-            Glide.with(baseContext).asBitmap().load(spotify).into(SpotifyPrize)
-
-            Thread.sleep(10)
-
-            var rio =
-                "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fstay_prize.jpeg?alt=media&token=9c6f0296-64b0-4997-b7b4-97cc45f9efa2"
-            Glide.with(baseContext).asBitmap().load(rio).into(Rio)
-
-            Thread.sleep(10)
-
-            var mex =
-                "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fstay_prize.jpeg?alt=media&token=9c6f0296-64b0-4997-b7b4-97cc45f9efa2"
-            Glide.with(baseContext).asBitmap().load(mex).into(Mexico)
-
-            Thread.sleep(10)
-
-            var video =
-                "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fstay_prize.jpeg?alt=media&token=9c6f0296-64b0-4997-b7b4-97cc45f9efa2"
-            Glide.with(baseContext).asBitmap().load(video).into(VideoGame)
-
-
-            NetflixPrize.setOnClickListener(){
-
-                Toast.makeText(this@MainActivity,"Successfully written",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity,"Good choice",Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, Choice::class.java)
-                image = "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fstay_prize.jpeg?alt=media&token=9c6f0296-64b0-4997-b7b4-97cc45f9efa2"
+                image = "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2FComponent%201.png?alt=media&token=859b98f4-a6dd-4edd-a678-367f397dd201"
                 intent.putExtra("img", image)
                 startActivity(intent)
-            }}
+            }
+
+            binding.GooglePlayPrize.setOnClickListener(){
+
+                Toast.makeText(this@MainActivity,"Good choice",Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, Choice::class.java)
+                image = "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fgoogle%20play.png?alt=media&token=c0bce8e7-9b3d-4597-9233-975262ba35bb"
+                intent.putExtra("img", image)
+                startActivity(intent)
+            }
+
+            binding.SpotifyPrize.setOnClickListener(){
+
+                Toast.makeText(this@MainActivity,"Good choice",Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, Choice::class.java)
+                image = "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fspotify.png?alt=media&token=c3facf8c-b820-44c8-92dd-8e87ac950798"
+                intent.putExtra("img", image)
+                startActivity(intent)
+            }
+
+            binding.PrimePrize.setOnClickListener(){
+
+                Toast.makeText(this@MainActivity,"Good choice",Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, Choice::class.java)
+                image = "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fprime.png?alt=media&token=b093fa27-bf0e-4460-825b-e083fa6399fb"
+                intent.putExtra("img", image)
+                startActivity(intent)
+            }
+
+            binding.TravelPrize.setOnClickListener(){
+
+                Toast.makeText(this@MainActivity,"Good choice",Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, Choice::class.java)
+                image = "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Ftravel.png?alt=media&token=36e86672-8e58-4aec-b5e3-3333040d3f88"
+                intent.putExtra("img", image)
+                startActivity(intent)
+            }
+
+            binding.TokyoPrize.setOnClickListener(){
+
+                Toast.makeText(this@MainActivity,"Good choice",Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, Choice::class.java)
+                image = "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Ftokyo.png?alt=media&token=3f6290a9-4845-4378-b852-acd47a7a3062"
+                intent.putExtra("img", image)
+                startActivity(intent)
+            }
+
+            binding.LondonPrize.setOnClickListener(){
+
+                Toast.makeText(this@MainActivity,"Good choice",Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, Choice::class.java)
+                image = "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Flondon.jpeg?alt=media&token=67ab9081-7091-4703-bba5-721d38942645"
+                intent.putExtra("img", image)
+                startActivity(intent)
+            }
 
 
-            private fun cinema(){
-                var cinema =
-                    "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fphoto-1574267432553-4b4628081c31.jpeg?alt=media&token=39dc7762-a5e5-41ad-8e6c-727680154b41"
-                Glide.with(baseContext).asBitmap().load(cinema).into(binding.NetflixPrize)}
+            binding.CastlePrize.setOnClickListener(){
+
+                Toast.makeText(this@MainActivity,"Good choice",Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, Choice::class.java)
+                image = "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fcastle.png?alt=media&token=4140b6fc-576b-4fef-8398-5e9cc29f1a1f"
+                intent.putExtra("img", image)
+                startActivity(intent)
+            }
+
+
+            binding.Best1.setOnClickListener(){
+
+                Toast.makeText(this@MainActivity,"Good choice",Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, Choice::class.java)
+                image = "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fgoogle%20play.png?alt=media&token=c0bce8e7-9b3d-4597-9233-975262ba35bb"
+                intent.putExtra("img", image)
+                startActivity(intent)
+            }
+
+            binding.Best2.setOnClickListener(){
+
+                Toast.makeText(this@MainActivity,"Good choice",Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, Choice::class.java)
+                image = "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Flondon.jpeg?alt=media&token=67ab9081-7091-4703-bba5-721d38942645"
+                intent.putExtra("img", image)
+                startActivity(intent)
+            }
+
+            binding.Best3.setOnClickListener(){
+
+                Toast.makeText(this@MainActivity,"Good choice",Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, Choice::class.java)
+                image = "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Ftokyo.png?alt=media&token=3f6290a9-4845-4378-b852-acd47a7a3062"
+                intent.putExtra("img", image)
+                startActivity(intent)
+            }
+
+            binding.Best4.setOnClickListener(){
+
+                Toast.makeText(this@MainActivity,"Good choice",Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, Choice::class.java)
+                image = "https://.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fxbox.png?alt=media&token=7009da2f-7ff4-4e7c-a391-377097016e67"
+                intent.putExtra("img", image)
+                startActivity(intent)
+            }
+
         }
+
+
+
+
+      private fun googleplay() {
+          var googlePlay =
+                    "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fgoogle%20play.png?alt=media&token=c0bce8e7-9b3d-4597-9233-975262ba35bb"
+          Glide.with(baseContext).asBitmap().load(googlePlay).into(binding.GooglePlayPrize)
+            }
+
+     private fun netflix() {
+         var netflix =
+             "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2FComponent%201.png?alt=media&token=859b98f4-a6dd-4edd-a678-367f397dd201"
+         Glide.with(baseContext).asBitmap().load(netflix).into(binding.NetflixPrize)
+        }
+
+     private fun spotify() {
+         var spot =
+             "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fspotify.png?alt=media&token=c3facf8c-b820-44c8-92dd-8e87ac950798"
+         Glide.with(baseContext).asBitmap().load(spot).into(binding.SpotifyPrize)
+     }
+
+     private fun prime() {
+         var prime =
+             "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fprime.png?alt=media&token=b093fa27-bf0e-4460-825b-e083fa6399fb"
+         Glide.with(baseContext).asBitmap().load(prime).into(binding.PrimePrize)
+     }
+
+     private fun travel() {
+         var trav =
+             "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Ftravel.png?alt=media&token=36e86672-8e58-4aec-b5e3-3333040d3f88"
+         Glide.with(baseContext).asBitmap().load(trav).into(binding.TravelPrize)
+     }
+
+     private fun tokyo() {
+         var tok =
+             "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Ftokyo.png?alt=media&token=3f6290a9-4845-4378-b852-acd47a7a3062"
+         Glide.with(baseContext).asBitmap().load(tok).into(binding.TokyoPrize)
+     }
+
+     private fun london() {
+         var lond =
+             "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Flondon.jpeg?alt=media&token=67ab9081-7091-4703-bba5-721d38942645"
+         Glide.with(baseContext).asBitmap().load(lond).into(binding.LondonPrize)
+     }
+
+     private fun castle() {
+         var castle =
+             "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fcastle.png?alt=media&token=4140b6fc-576b-4fef-8398-5e9cc29f1a1f"
+         Glide.with(baseContext).asBitmap().load(castle).into(binding.CastlePrize)
+     }
+
+     private fun best1() {
+         var b1 =
+             "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fgoogle%20play.png?alt=media&token=c0bce8e7-9b3d-4597-9233-975262ba35bb"
+         Glide.with(baseContext).asBitmap().load(b1).into(binding.Best1)
+     }
+
+     private fun best2() {
+         var b2 =
+             "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Flondon.jpeg?alt=media&token=67ab9081-7091-4703-bba5-721d38942645"
+         Glide.with(baseContext).asBitmap().load(b2).into(binding.Best2)
+     }
+
+     private fun best3() {
+         var b3 =
+             "https://firebasestorage.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Ftokyo.png?alt=media&token=3f6290a9-4845-4378-b852-acd47a7a3062"
+         Glide.with(baseContext).asBitmap().load(b3).into(binding.Best3)
+     }
+
+     private fun best4() {
+         var b4 =
+             "https://.googleapis.com/v0/b/wheeler-d6e1d.appspot.com/o/Image%2Fxbox.png?alt=media&token=7009da2f-7ff4-4e7c-a391-377097016e67"
+         Glide.with(baseContext).asBitmap().load(b4).into(binding.Best4)
+     }
+
+ }
+
+
+
 
