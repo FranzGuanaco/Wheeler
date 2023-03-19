be61095f88765fb8fe3ff4204ee24cc63dcb3727
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2023-03-19 22:40:09 +0100

    dar mode 1/2

--- app/src/main/java/com/View/ChangePassword.kt
+++ app/src/main/java/com/View/ChangePassword.kt
@@ -1,11 +1,12 @@
-package com.example.wheeler
+package com.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wheeler.R
 
-class Security : AppCompatActivity() {
+class ChangePassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_security)
+        setContentView(R.layout.activity_change_password)
     }
 }
